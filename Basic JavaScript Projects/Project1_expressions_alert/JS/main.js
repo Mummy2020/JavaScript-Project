708afbe17518ccbfa72943be2421e2a01d5252a3
Mummy2020 <128103745+Mummy2020@users.noreply.github.com>
2023-04-06 12:30:10 -0700

    My JavaScript project
    This file will contain all of my projects in JavaScript course

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+document.write("Hellow, World <br>"); //This code is printing the word hellow world on the screen
+var A = "This is the beginning of string<br>"; //This is a variable that stores a string value
+var B = "and this is the end of string<br>" //This is a variable that stores a string value
+document.write(A); //This code prints the value of variable A on the screen
+document.write(B), //This code prints the value of variable A on the screen
+window.alert(A); // This code pops-up window on the sreen with an ok button
+document.write("\"Don't you think Marry was little stubborn!\" Eva said <br>"); //This code shows how to put quotes in JavaScript 
+document.write("My name is Sankung Saidyleigh " + " I am from the Gambia, West Africa " + " I am married and have beautiful children <br>"); //This code shows how to concatenate strings
+Family = "Saidyleigh's", Dad = "Sankung", Mom = "Isha", Daughter = "Fatima and Mariam", Son = "Bubacarr<br>"; //This code shows how to assign multiple variables 
+document.write(Son); //This code prints the value of the variable Son on the screen.
+document.write(10+10+10); //This show an expression 
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
